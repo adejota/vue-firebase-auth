35833e01d1540af3ef0a9ca02a869600a7bc27e4
Ademir Diamente Júnior <ademir.diamentejunior@gmail.com>
2021-06-01 14:56:00 -0300

    README

--- README.md
+++ README.md
@@ -5,6 +5,31 @@
 npm install
 ```
 
+### Setup firebase 🔥
+Go to [Firebase console](https://console.firebase.google.com/):
+```
++ Add project
+Enter your project name
+Disable Google Analytics
+Create project
+```
+
+Once the project is created, inside Project Overview, add Firebase to your web app (</>):
+```
+Add an app nickname
+Register app
+```
+
+Finally, create an .env file at the root of your project and add Firebase config:
+```
+VUE_APP_API_KEY=
+VUE_APP_AUTH_DOMAIN=
+VUE_APP_PROJECT_ID=
+VUE_APP_STORAGE_BUCKET=
+VUE_APP_MESSAGING_SENDER_ID=
+VUE_APP_APP_ID=
+```
+
 ### Compiles and hot-reloads for development
 ```
 npm run serve
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,31 @@
 <template>
     <v-main class="px-4 py-8">
-        <!-- <p class="text-h5">User information</p> -->
+        <div class="d-flex justify-center">
+            <v-responsive max-width="500px">
+                <div class="d-flex flex-column align-center mb-4">
+                    <v-avatar size="100" color="primary">
+                        <v-img v-if="this.getUser.photoURL" 
+                            :src="this.getUser.photoURL"
+                        />
+                        <span v-else class="white--text text-h4 text-uppercase">{{ userInitials(this.getUser.email) }}</span>
+                    </v-avatar>
+                </div>
 
-        <div class="d-flex flex-column align-center mb-4">
-            <v-avatar size="100">
-                <v-img v-if="this.getUser.photoURL" 
-                    :src="this.getUser.photoURL"
-                />
-                <v-icon v-else large color="primary">mdi-account-circle</v-icon>
-            </v-avatar>
-        </div>
+                <div class="d-flex flex-column align-start mb-4">
+                    <strong><span>Name:</span></strong>
+                    <span>{{ this.getUser.displayName ? this.getUser.displayName : 'Uninformed' }}</span>
+                </div>
 
-        <div class="d-flex flex-column align-start mb-4">
-            <strong><span>Name:</span></strong>
-            <span>{{ this.getUser.displayName ? this.getUser.displayName : 'Uninformed' }}</span>
-        </div>
+                <div class="d-flex flex-column mb-4">
+                    <strong><span>E-mail:</span></strong>
+                    <span>{{ this.getUser.email }}</span>
+                </div>
 
-        <div class="d-flex flex-column mb-4">
-            <strong><span>E-mail:</span></strong>
-            <span>{{ this.getUser.email }}</span>
-        </div>
-
-        <div class="d-flex flex-column mb-4">
-            <strong><span>Phone number:</span></strong>
-            <span>{{ this.getUser.phoneNumber ? this.getUser.phoneNumber : 'Uninformed' }}</span>
+                <div class="d-flex flex-column mb-4">
+                    <strong><span>Phone number:</span></strong>
+                    <span>{{ this.getUser.phoneNumber ? this.getUser.phoneNumber : 'Uninformed' }}</span>
+                </div>
+            </v-responsive>
         </div>
     </v-main>
 </template>
@@ -42,6 +44,13 @@ export default {
 
     computed: {
         ...mapGetters(['getUser', 'isUserAuth'])
+    },
+
+    methods: {
+        userInitials(email) {
+            let _email = email.split('')
+            return _email[0] + _email[1]
+        }
     },
 
     mounted() {
